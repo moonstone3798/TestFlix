8d96e2af156b6a6a86dc8f9bbdd1bfdbd7d92b54
moonstone3798 <moonstone3798@gmail.com>
2025-01-08 16:41:52 -0300

    I changed some values of responsive dessign

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -4,8 +4,8 @@ import SliderComponent from './SliderComponent.vue'
 </script>
 
 <template>
-  <div class="flex md:flex-row flex-col md:items-end">
-    <div class="md:w-3/4 md:pl-24 pt-32 md:pt-0 flex md:block flex-col items-center">
+  <div class="flex md:flex-row flex-col md:items-end md: justify-around">
+    <div class="md:w-[51vw] pt-32 md:pb-[10.79vh] md:pt-0 flex md:block flex-col items-center">
       <h4 class="text-white text-base md:text-xl font-medium leading-6">ORIGINAL DE TESTFLIX</h4>
       <h1
         class="text-[#D1A2FF] tracking-[1rem] md:text-[5vw] text-4xl text-center md:text-left md:text-[4.3em] font-medium md:leading-[100px] leading-[77px]"
@@ -21,13 +21,14 @@ import SliderComponent from './SliderComponent.vue'
           :class="'bg-[#24242480] 	'"
         />
         <button
+          title="Añadir a Mi Lista"
           class="bg-[#242424] md:hidden block border px-2 border-white rounded-[5px] text-white flex items-center h-14 justify-center gap-3.5"
         >
           <span class="icon-[mynaui--plus] text-2xl" role="img" aria-hidden="true"></span>
         </button>
       </div>
     </div>
-    <div class="md:w-1/4 min-h-[80vh] bg-[#242424] md:bg-transparent pb-14 md:pb-0 mt-4">
+    <div class="bg-[#242424] md:bg-transparent pb-14 md:pb-0 mt-3">
       <SliderComponent />
     </div>
   </div>
