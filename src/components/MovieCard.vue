a5c93fcd44846e4e2a333177ef80c0172b8909f2
moonstone3798 <moonstone3798@gmail.com>
2025-01-08 16:42:51 -0300

    I implemented the border radius for the Movie Card

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 <template>
   <div
     :style="{ backgroundImage: `url(/img/${props.img})` }"
-    class="flex flex-col items-center lg:w-[15vw] md:w-[22vw] md:h-[16vh] h-[27vh] w-[87vw] bg-cover justify-center gap-2"
+    class="flex flex-col rounded-[4px] items-center lg:w-[15.27vw] md:w-[22vw] md:h-[15.66vh] h-[27vh] w-[87vw] bg-cover justify-center gap-2"
   >
     <IconPlay />
     <p v-text="props.name" class="font-medium text-base leading-4 tracking-[4px] text-white" />
