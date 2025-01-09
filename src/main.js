e796f30a892f2f86878b80365d9525fbf15f65af
moonstone3798 <moonstone3798@gmail.com>
2025-01-09 13:45:27 -0300

    I added router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import 'preline'
 import './assets/main.css'
 import { createApp } from 'vue'
+import router from './router'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/router.js
@@ -0,0 +1,31 @@
+import { createWebHistory, createRouter } from 'vue-router'
+
+// import MainComponent from './components/MainComponent.vue'
+import SliderComponent from './components/SliderComponent.vue'
+const routes = [
+  {
+    path: '/',
+    redirect: '/tendencia',
+  },
+  {
+    path: '/tendencia',
+    name: 'TENDENCIA',
+    component: SliderComponent,
+  },
+  {
+    path: '/mis-peliculas',
+    name: 'MIS PELICULAS',
+    component: SliderComponent,
+  },
+  {
+    path: '/agregadas-recientemente',
+    name: 'AGREGADAS RECIENTEM.',
+    component: SliderComponent,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+export default router
